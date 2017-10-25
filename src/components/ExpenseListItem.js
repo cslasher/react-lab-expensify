50fc83c69a3e2e94d7437aade5b81fc4c1cec6cf
Edwin Liu <cslasher@gmail.com>
2017-10-25 19:21:22 +0800

    Implement filter box and remove

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const Item = ({ description, amount, createdAt }) => (
+const Item = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h4>{description}</h4>
     <p>
       ${amount} - {createdAt}
     </p>
+    <button
+      onClick={e => {
+        dispatch(removeExpense(id));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default Item;
+export default connect()(Item);
