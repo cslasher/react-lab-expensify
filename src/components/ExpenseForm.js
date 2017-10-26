9ae4b98d7f44850ae747a416ec2e85237ab5977a
Edwin Liu <cslasher@gmail.com>
2017-10-26 15:01:05 +0800

    Implement add expense date picker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     amount: '',
-    note: ''
+    note: '',
+    createAt: moment(),
+    calendarFocused: false
   };
   onDescriptionChange = e => {
     const description = e.target.value;
@@ -15,6 +21,12 @@ export default class ExpenseForm extends React.Component {
     if (amount.match(/^\d*(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
+  };
+  onDateChange = createAt => {
+    this.setState(() => ({ createAt }));
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
   };
   onNoteChange = e => {
     const note = e.target.value;
@@ -36,6 +48,14 @@ export default class ExpenseForm extends React.Component {
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expense(optional)"
