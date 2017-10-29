7db4664bc0f09e823dc8ed386d5d72179fcb627a
Edwin Liu <cslasher@gmail.com>
2017-10-30 01:11:14 +0800

    Extract css

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = env => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
   return {
     entry: './src/app.js',
     output: {
@@ -17,11 +20,27 @@ module.exports = env => {
         },
         {
           test: /\.s?css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
       ]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
