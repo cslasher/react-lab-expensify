7e863c8e8b6bdc4fbc4115c28d9768e0f7b1fa2e
Edwin Liu <cslasher@gmail.com>
2017-10-29 19:06:38 +0800

    Refactor AddExpensePage and test

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,14 +3,16 @@ import { shallow } from 'enzyme';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixures/expenses';
 
-let onSubmit;
+let addExpense;
 let history;
 let wrapper;
 
 beforeEach(() => {
-  onSubmit = jest.fn();
+  addExpense = jest.fn();
   history = { push: jest.fn() };
-  wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
+  wrapper = shallow(
+    <AddExpensePage addExpense={addExpense} history={history} />
+  );
 });
 
 test('should render AddExpensePage correctly', () => {
@@ -20,5 +22,5 @@ test('should render AddExpensePage correctly', () => {
 test('should handle on submit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
