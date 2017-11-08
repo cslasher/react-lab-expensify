4d4dab059439ef35db3d342eb24e32236901c69e
Edwin Liu <cslasher@gmail.com>
2017-11-09 00:53:47 +0800

    Link multiple providers to one account

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -11,13 +11,56 @@ export const login = uid => ({
 
 export const startGoogleLogin = () => {
   return () => {
-    return firebase.auth().signInWithPopup(googleAuthProvider);
+    return firebase
+      .auth()
+      .signInWithPopup(googleAuthProvider)
+      .catch(error => {
+        if (error.code === 'auth/account-exists-with-different-credential') {
+          const pendingCred = error.credential;
+          return firebase
+            .auth()
+            .signInWithRedirect(githubAuthProvider)
+            .then(result => {
+              return result.user;
+            })
+            .then(user => {
+              return user.linkWithCredential(pendingCred);
+            });
+        }
+        throw error;
+      });
   };
 };
 
 export const startGithubLogin = () => {
   return () => {
-    return firebase.auth().signInWithPopup(githubAuthProvider);
+    return firebase
+      .auth()
+      .signInWithPopup(githubAuthProvider)
+      .catch(error => {
+        if (error.code === 'auth/account-exists-with-different-credential') {
+          const existingEmail = error.email;
+          const pendingCred = error.credential;
+          return firebase
+            .auth()
+            .fetchProvidersForEmail(error.email)
+            .then(providers => {
+              googleAuthProvider.setCustomParameters({
+                login_hint: existingEmail
+              });
+              return firebase
+                .auth()
+                .signInWithRedirect(googleAuthProvider)
+                .then(result => {
+                  return result.user;
+                });
+            })
+            .then(user => {
+              return user.linkWithCredential(pendingCred);
+            });
+        }
+        throw error;
+      });
   };
 };
 
